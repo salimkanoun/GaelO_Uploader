df41a04ad53a2d0585c9fe3e3ca75bc2b814eb60
Emilie <emilie.olivie@hotmail.fr>
2020-06-29 10:17:58 +0200

    redux removed

--- gaelo-uploader/src/actions/actions-type.js
+++
@@ -1,1 +0,0 @@
-export const SELECT_STUDY = 'SELECT_STUDY'
--- gaelo-uploader/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import Uploader from './Uploader'
-
-export default combineReducers({
-    Uploader
-})

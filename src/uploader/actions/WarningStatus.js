72663dcc81c8f0b63f8788136db16081ddb1514b
Emilie <emilie.olivie@hotmail.fr>
2020-07-08 12:26:49 +0200

    commit

--- src/uploader/actions/WarningStatus.js
+++
@@ -1,8 +0,0 @@
-import { UPDATE_WARNING_OBJECT } from './actions-types'
-
-export function updateWarningObject(warningToUpdate){
-    return {
-        type: UPDATE_WARNING_OBJECT, 
-        payload: warningToUpdate
-    }
-}

42cf76057790f201fefe94dc0b23973f55298b83
Emilie <emilie.olivie@hotmail.fr>
2020-07-08 12:26:36 +0200

    redux dev tool added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import index from './uploader/reducers'
 
-const store = createStore(index)
+const store = createStore(index, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>

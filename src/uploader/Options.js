5cc77e2799e8cdcfa9e3da67205b22e458bb58d8
Salim Kanoun <salim.kanoun@gmail.com>
2020-08-13 02:06:14 +0200

    debug

--- src/uploader/Options.js
+++ src/uploader/Options.js
@@ -15,7 +15,7 @@
 import React, { Component } from 'react'
 import { Popover, OverlayTrigger, Button } from 'react-bootstrap'
 import { Slider, Typography } from '@material-ui/core';
-import { OptionCog } from '../style/images/optionCog.svg'
+import OptionCog from '../images/optionCog'
 export default class Options extends Component {
 
     state = {
--- webpack.config.js
+++ webpack.config.js
@@ -26,18 +26,8 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        use: [
-          {
-            loader: "babel-loader"
-          },
-          {
-            loader: "react-svg-loader",
-            options: {
-              jsx: true // true outputs JSX tags
-            }
-          }
-        ]
-      }
+        use: ['@svgr/webpack'],
+      },
     ]
   },
   resolve: {

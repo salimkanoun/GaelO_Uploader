70aecb62afbd0dcaed93d6cd1a08778f4fe7961d
Salim Kanoun <salim.kanoun@gmail.com>
2021-03-13 00:36:46 +0100

    progress internface

--- src/uploader/render_component/DicomRTEdition.js
+++ src/uploader/render_component/DicomRTEdition.js
@@ -12,38 +12,62 @@
  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
- import React, { Component, Fragment } from 'react'
- import { connect } from 'react-redux'
- 
- import BootstrapTable from 'react-bootstrap-table-next';
- 
- 
- class DicomRTEdition extends Component {
- 
-     columns = [
-             {
-                 dataField: 'ROIName',
-                 text: 'Roi Name',
-                 editable: false
-             },
-             {
-                 dataField: 'ROINumber',
-                 text: 'Roi Number',
-                 editable: false
-             },
-         ]
- 
-     selectRow = {
-         mode: 'radio',
-         clickToSelect: true,
-         clickToEdit: true,
-         hideSelectColumn: true,
-         classes: "row-clicked"
-     }
- 
-     render() {
-         return (
-             <Fragment>
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+
+import BootstrapTable from 'react-bootstrap-table-next'
+
+class DicomRTEdition extends Component {
+
+    state = {
+        newROIName : {}
+    }
+
+    columns = [
+        {
+            dataField: 'ROIName',
+            text: 'ROI Name',
+            editable: false
+        },
+        {
+            dataField: 'ROINumber',
+            text: 'Roi Number',
+            editable: false
+        },
+        {
+            dataField: 'newName',
+            text: 'New ROI Name',
+            isDummyField : true,
+            formatter : (cell, row, rowIndex) => {
+
+                let onChange = (event)=>{
+                    this.setState((state) => {
+                        state['newROIName'][row.ROINumber] = event.target.value
+                        return state
+                    }, () => {console.log(this.state)})
+
+                }
+
+                return (
+                    <input type="text" onChange={onChange} value={this.state.newROIName[row.ROINumber]}/>
+                )
+
+            }
+        }
+    ]
+
+    selectRow = {
+        mode: 'radio',
+        clickToSelect: true,
+        clickToEdit: true,
+        hideSelectColumn: true,
+        classes: "row-clicked"
+    }
+
+    render() {
+        console.log(this.props.series[this.props.selectedSeries]['structureSetROISequence'])
+        return (
+            <Fragment>
                 <BootstrapTable
                     keyField='ROINumber'
                     classes="table table-borderless"
@@ -51,17 +75,17 @@
                     data={this.props.series[this.props.selectedSeries]['structureSetROISequence']}
                     columns={this.columns}
                     selectRow={this.selectRow} />
-             </Fragment>
-         )
-     }
- }
- 
- 
- const mapStateToProps = state => {
-     return {
-         selectedSeries: state.DisplayTables.selectedSeries,
-         series: state.Series.series
-     }
- }
- 
- export default connect(mapStateToProps, null)(DicomRTEdition)
+            </Fragment>
+        )
+    }
+}
+
+
+const mapStateToProps = state => {
+    return {
+        selectedSeries: state.DisplayTables.selectedSeries,
+        series: state.Series.series
+    }
+}
+
+export default connect(mapStateToProps, null)(DicomRTEdition)
